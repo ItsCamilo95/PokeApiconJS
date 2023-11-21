4cd2f2e688ac42a7553b950a5160cba8afbd8aae
Camilo <camiloorozco2001@gmail.com>
2023-11-21 01:28:32 -0500

    Api consumida para traer listado de pokemones, listos los estilos de la lista de pokemones incluida navegación de 20 en 20, falta añadir detalle de cada pokemon

--- pokemon.js
+++ pokemon.js
@@ -1,40 +1,54 @@
-let pokeContainer = document.querySelector("#pokeContainer");
+let pokeList = document.querySelector("#pokeList");
 let inputSearchPokemon = document.querySelector("#searchPokemon");
 let pokeURL = "https://pokeapi.co/api/v2/pokemon/";
-let pokeArray = [];
-let pokemonValue = '';
 let searchButton = document.querySelector("#searchButton");
-const loadPokemones = () => {
-    console.log("El contenido está cargando")
-    fetch( pokeURL )
+let listControl = document.querySelector(".list-control");
+let next = null;
+let previous = null;
+let pokemonValue = '';
+
+const loadPokemones = (URL) => {
+    fetch( URL )
 			.then( respuesta => respuesta.json() )
 			.then( datos => {
-                console.log("Ya cargó papi");
-				pokeArray = datos.results
-				console.log(pokeArray);
+				pokeList.innerHTML='';
+                for (let i = 0; i < datos.results.length; i++) {
+					pokeList.innerHTML+= `<li><button>${datos.results[i].url.replace('https://pokeapi.co/api/v2/pokemon/', '').replace('/',' - ')}${datos.results[i].name}</button></li>`;
+				}
+				pokeList.scrollIntoView({behavior:"smooth"});
+				previous = datos.previous;
+				next = datos.next;
+				listControl.innerHTML=`<button class="control-buttons" onclick="previousList('${previous}')">Anterior</button>
+				<button class="control-buttons" onclick="nextList('${next}')">Siguiente</button>`;
+				
 				//pokeURL = datos.next;
 				//si el usuario retrocede, usar previus, si adelanta, usar next, y listo.
 			});
 }
 
 const searchPokemon = (pokemon) => {
-	console.log("El contenido está cargando")
-    fetch( pokeURL+pokemon )
-			.then( respuesta => respuesta.json() )
-			.then( datos => {
-                console.log("Ya cargó papi");
-				console.log(datos.srp);
-			});
+	listControl.innerHTML="";
+    fetch( pokeURL+pokemon)
+		.then( respuesta => respuesta.json() )
+		.then( datos => {
+			pokeList.innerHTML='';
+			pokeList.innerHTML+= `<li><button style="text-transform: capitalize;">${datos.id+" - "+datos.name}</button></li>`;
+		})
+		.catch(notFound);
+}
+
+const notFound = () => {
+	pokeList.innerHTML= '<div style="color: red;">No encontrado</div>';
 }
 
 inputSearchPokemon.addEventListener('keyup', (event) => {
 	if (event.key === 'Enter') {
-	const pokemonValue = inputSearchPokemon.value;
-	if (pokemonValue !== '') {
-		searchPokemon(pokemonValue);
-	} else {
-		loadPokemones();
-	}
+		const pokemonValue = inputSearchPokemon.value;
+		if (pokemonValue !== '') {
+			searchPokemon(pokemonValue);
+		} else {
+			loadPokemones(pokeURL);
+		}
 	}
 });
 
@@ -43,8 +57,18 @@ searchButton.addEventListener('click', (event) => {
 	if (pokemonValue !== '') {
 	searchPokemon(pokemonValue);
 	} else {
-	loadPokemones();
+	loadPokemones(pokeURL);
 	}
 });
 
+function nextList (url) {
+	if(url !== 'null'){
+		loadPokemones(url)
+	}
+}
 
+function previousList (url) {
+	if(url !== 'null'){
+		loadPokemones(url)
+	}
+}
